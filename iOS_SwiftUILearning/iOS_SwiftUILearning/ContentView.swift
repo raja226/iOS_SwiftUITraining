f025cb1e090b980362518dc908e642d455b3392c
raja226 <iosrajasekhar@gmail.com>
2024-07-14 13:56:28 +0530

    initial scroll

--- iOS_SwiftUILearning/iOS_SwiftUILearning/ContentView.swift
+++ iOS_SwiftUILearning/iOS_SwiftUILearning/ContentView.swift
@@ -9,15 +9,58 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-                .padding()
-                .background(.green)
+        ScrollView
+        {
+            VStack
+            {
+                
+                Text("LaswinRaj1")
+                    .padding()
+                Text("LaswinRaj2")
+                    .padding()
+                Text("LaswinRaj3")
+                    .padding()
+                Text("LaswinRaj4")
+                    .padding()
+                Text("LaswinRaj5")
+                    .padding()
+                Text("LaswinRaj6")
+                    .padding()
+                Text("LaswinRaj7")
+                    .padding()
+                Text("LaswinRaj8")
+                    .padding()
+                Text("LaswinRaj9")
+                    .padding()
+                Text("LaswinRaj10")
+                    .padding()
+                
+                Text("LaswinRaj1")
+                    .padding()
+                Text("LaswinRaj2")
+                    .padding()
+                Text("LaswinRaj3")
+                    .padding()
+                Text("LaswinRaj4")
+                    .padding()
+                Text("LaswinRaj5")
+                    .padding()
+                Text("LaswinRaj6")
+                    .padding()
+                Text("LaswinRaj7")
+                    .padding()
+                Text("LaswinRaj8")
+                    .padding()
+                Text("LaswinRaj9")
+                    .padding()
+                Text("LaswinRaj10")
+                    .padding()
+            }
+            .padding()
         }
         .padding()
+
+       
     }
 }
 
